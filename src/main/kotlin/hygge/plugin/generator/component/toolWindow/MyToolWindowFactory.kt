01efa3f2a1899355f306fc08546dbd6a3bf9d90e
Xavier.Wang <xavierpe@qq.com>
2024-06-04 22:34:32 +0800

    调整 UI

--- src/main/kotlin/hygge/plugin/generator/component/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/hygge/plugin/generator/component/toolWindow/MyToolWindowFactory.kt
@@ -1,16 +1,24 @@
 package hygge.plugin.generator.component.toolWindow
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.ui.ComboBox
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.EnumComboBoxModel
+import com.intellij.ui.TitledSeparator
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
+import com.intellij.ui.components.JBPasswordField
+import com.intellij.ui.components.JBTextField
 import com.intellij.ui.content.ContentFactory
+import com.jetbrains.rd.swing.selectedItemProperty
 import hygge.plugin.generator.component.toolWindow.service.MyToolWindowService
 import hygge.plugin.generator.core.domain.bo.DatabaseConfiguration
 import hygge.plugin.generator.core.service.topo.PoGenerator
+import hygge.plugin.generator.language.LanguageEnum
 import hygge.plugin.generator.util.BundleUtil
 import hygge.util.UtilCreator
 import hygge.util.constant.ConstantClassInfoContainer
@@ -19,6 +27,7 @@ import hygge.util.generator.java.bo.ClassInfo
 import hygge.util.generator.java.bo.Modifier
 import hygge.util.generator.java.bo.Property
 import javax.swing.JButton
+
 
 /**
  * 新建一个工具栏(和 project 栏类似)，中间包含一个按钮，每次点击生成一个随机数并切换语言
@@ -31,24 +40,69 @@ class MyToolWindowFactory : ToolWindowFactory {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val myToolWindow = MyToolWindow(toolWindow)
-        val content = ContentFactory.getInstance().createContent(myToolWindow.getContent(project), null, false)
+
+        myToolWindow.bindComponent(project)
+
+        val content = ContentFactory.getInstance().createContent(myToolWindow.mainContent, null, false)
+
         toolWindow.contentManager.addContent(content)
     }
 
     override fun shouldBeAvailable(project: Project) = true
 
     class MyToolWindow(toolWindow: ToolWindow) {
-
         private val service = toolWindow.project.service<MyToolWindowService>()
+        val mainContent: JBPanel<JBPanel<*>> = JBPanel<JBPanel<*>>()
+        val languageComboBox = ComboBox(EnumComboBoxModel(LanguageEnum::class.java))
+        val databaseConfigurationTitle: TitledSeparator = TitledSeparator()
+        val userNameJBLabel: JBLabel = JBLabel()
+        val userNameJBTextField: JBTextField = JBTextField()
+        val passwordJBLabel: JBLabel = JBLabel()
+        val passwordJBPasswordField: JBPasswordField = JBPasswordField()
+        val generateButton = JButton()
 
-        fun getContent(project: Project) = JBPanel<JBPanel<*>>().apply {
-            val labelInitText: String = BundleUtil.message(service.getCurrentLanguage(), "randomLabel", "?")
-            val label = JBLabel(labelInitText)
+        fun bindComponent(project: Project) {
+            refreshText(service.getCurrentLanguage())
 
-            val button = createButton(project, label)
+            mainContent.add(databaseConfigurationTitle)
 
-            add(label)
-            add(button)
+            mainContent.add(userNameJBLabel)
+            mainContent.add(userNameJBTextField)
+
+            mainContent.add(passwordJBLabel)
+            mainContent.add(passwordJBPasswordField)
+
+            // 点击生成按钮时，进行代码自动生成
+            generateButton.addActionListener {
+                val jsonHelper = UtilCreator.INSTANCE.getDefaultJsonHelperInstance<ObjectMapper>(true)
+                val databaseConfiguration = DatabaseConfiguration(
+                    project.basePath!!,
+                    "localhost",
+                    userNameJBTextField.text,
+                    String(passwordJBPasswordField.password),
+                    "/local_test"
+                )
+                println(jsonHelper.formatAsString(databaseConfiguration))
+                // 生成代码
+            }
+            mainContent.add(generateButton)
+
+            // 检测到语言切换时刷新组件文本
+            languageComboBox.addActionListener {
+                val comboBox: ComboBox<LanguageEnum> = it.source as ComboBox<LanguageEnum>
+                val currentType = comboBox.selectedItemProperty()
+                refreshText(currentType.value!!)
+            }
+
+            mainContent.add(languageComboBox)
+        }
+
+        fun refreshText(languageType: LanguageEnum) {
+            databaseConfigurationTitle.text = BundleUtil.message(languageType, "databaseConfigurationTitle")
+            userNameJBLabel.text = BundleUtil.message(languageType, "userNameJBLabel")
+            passwordJBLabel.text = BundleUtil.message(languageType, "passwordJBLabel")
+
+            generateButton.text = BundleUtil.message(languageType, "generateButton")
         }
 
         fun createButton(project: Project, label: JBLabel): JButton {
@@ -61,7 +115,8 @@ class MyToolWindowFactory : ToolWindowFactory {
                 service.setCurrentLanguage()
 
                 val schema = "local_test"
-                val databaseConfiguration = DatabaseConfiguration(project.basePath!!, "localhost:3306/", "root", "0000", schema)
+                val databaseConfiguration =
+                    DatabaseConfiguration(project.basePath!!, "localhost:3306/", "root", "0000", schema)
                 databaseConfiguration.enumElementInterval = 100
                 databaseConfiguration.isLombokEnable = true
 
@@ -69,7 +124,8 @@ class MyToolWindowFactory : ToolWindowFactory {
 
                 var abstractClassInfoContainer: MutableMap<String, ClassInfo> = HashMap()
 
-                val collectionHelper: CollectionHelper = UtilCreator.INSTANCE.getDefaultInstance(CollectionHelper::class.java)
+                val collectionHelper: CollectionHelper =
+                    UtilCreator.INSTANCE.getDefaultInstance(CollectionHelper::class.java)
 
                 val basePo = ClassInfo.builder()
                     .name("BasePo")
